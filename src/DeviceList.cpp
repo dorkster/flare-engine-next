cbf763cc73f912e1c8bac73560ecc5859956bb23
Igor Paliychuk <mansonigor@gmail.com>
2015-10-21 19:53:51 +0300

    fix text flipping, remove code that will not work under GLES2

--- src/DeviceList.cpp
+++ src/DeviceList.cpp
@@ -35,6 +35,7 @@ RenderDevice* getRenderDevice(const std::string& name) {
 	if (name != "") {
 		if (name == "sdl") return new SDLSoftwareRenderDevice();
 		else if (name == "sdl_hardware") return new SDLHardwareRenderDevice();
+		else if (name == "opengl") return new OpenGLRenderDevice();
 		else {
 			logError("DeviceList: Render device '%s' not found. Falling back to the default.", name.c_str());
 			return new SDLHardwareRenderDevice();
